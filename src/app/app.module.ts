52ee04ab51c7840d6a7f91aa9606d0746879737f
Eddie <eddie@edease.nl>
2022-10-03 10:21:59 +0200

    settings things up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SiteComponent } from './site/site.component';
+import { AdminComponent } from './admin/admin.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const appRoutes: Routes = [
+  { path: 'site', component: SiteComponent },
+  {
+    path: '',
+    redirectTo: 'site',
+    pathMatch: 'full'
+  },
+  { path: '**', component: SiteComponent }
+];
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(
+      appRoutes,
+      { useHash: false } // <-- debugging purposes only true
+    ),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
